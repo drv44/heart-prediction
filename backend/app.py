9dec8f44fdbe83b99475e7929938d7e7fa2a7227
drv44 <dhruvsarkar960@gmail.com>
2025-04-18 21:47:31 +0530

    Fixed app.py

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,15 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
 import joblib
+import os
 
+HERE = os.path.dirname(__file__)
+MODEL_PATH = os.path.abspath(os.path.join(HERE, '..', 'models', 'ensemble.pkl'))
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # Load the pre-trained VotingClassifier (ensemble model)
-voting_clf = joblib.load('./models/ensemble.pkl')
+voting_clf = joblib.load(MODEL_PATH)
 
 @app.route('/')
 def home():
