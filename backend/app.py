e63a83bbdfedc98736b4296eac487581e640a36d
drv44 <dhruvsarkar960@gmail.com>
2025-04-18 20:54:07 +0530

    Fixed path in app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # Load the pre-trained VotingClassifier (ensemble model)
-voting_clf = joblib.load('models/ensemble.pkl')
+voting_clf = joblib.load('./models/ensemble.pkl')
 
 @app.route('/')
 def home():
